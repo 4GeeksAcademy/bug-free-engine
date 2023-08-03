913454a7096e16e9452dd405bee1133157928e36
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2023-08-03 01:27:48 +0000

    :construction: recursividad!

--- functions.js
+++ functions.js
@@ -1,17 +1,17 @@
 
-// let multiplicarNumeros = (num1, num2) => {
+let multiplicarNumeros = (num1, num2) => {
 
-//     if (num1 && num2) {
-//         return num1 * num2
-//     }
-//     return "Ah ocurrido un error!"
-// }
+    if (num1 && num2) {
+        return num1 * num2
+    }
+    return "Ah ocurrido un error!"
+}
 
-// const sumarCallback = (cb, num3) => {
-//     console.log("inicia la suma!")
-//     let res = num3 + cb(3, 7) 
-//     return res + " un saludo!"
-// }
+const sumarCallback = (cb, num3) => {
+    console.log("inicia la suma!")
+    let res = num3 + cb(3, 7)
+    return res + " un saludo!"
+}
 
 // let resultado = sumarCallback(console.log, 9)
 let resultado = 3;
---
+++ recursividad.js
@@ -0,0 +1,12 @@
+
+// Nivel hardcore
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144.
+function fibo(pos) {
+    if (pos == 0)
+        return 0
+    if (pos <= 2)
+        return 1
+    return fibo(pos - 2) + fibo(pos - 1)
+}
+
+console.log(fibo(12))
