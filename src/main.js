0d6277fd7a3ec1c085e5582e533501d029ac4a07
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2023-08-15 01:30:25 +0000

    :art: Events Click, KeyPress & other stuff

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+let texto = "";
+let input = document.querySelector("#destiny")
+let title = document.querySelector("#title")
+input.addEventListener("keyup", controlledInput)
+
+function controlledInput(evento){
+    texto = evento.target.value;
+}
+
 
 function MagicEightBall() {
     this.answers = [
@@ -35,37 +44,46 @@ function MagicEightBall() {
     }
 }
 
+let count = 0;
+let eightBall = document.querySelector("#eightball")
+let triangle = document.createElement("div")
+let triangleContainer = document.createElement("div")
+triangleContainer.classList = "d-flex justify-content-center align-items-center border border-width-1 border-white rounded-circle shadow-sm text-white"
+triangleContainer.style.width = "45%"
+triangleContainer.style.height = "45%"
+triangleContainer.classList.add("text-white")
 
-setTimeout(() => {
-    let eightBall = document.querySelector("#eightball")
-    let triangle = document.createElement("div")
+triangle.classList = "d-flex justify-content-center text-center"
+triangle.id = "triangle"
+triangle.style.padding = "20px"
+let number = document.querySelector(".number")
 
-    let triangleContainer = document.createElement("div")
-    triangleContainer.classList = "d-flex justify-content-center align-items-center border border-width-1 border-white rounded-circle shadow-sm text-white"
-    triangleContainer.style.width = "45%"
-    triangleContainer.style.height = "45%"
-    triangleContainer.classList.add("text-white")
+const shakeBall = () => {
+    title.innerText = "";
+    setTimeout(() => {
+        console.log(triangle)
+        if(count == 0 ){
+            eightBall.removeChild(number)
+            triangle.innerText = new MagicEightBall().generatePrediction()
+            triangleContainer.appendChild(triangle)
+            eightBall.appendChild(triangleContainer)
+            count++;
+        }else{
+            triangle.innerText = new MagicEightBall().generatePrediction()
+        }
+        title.innerText = texto;
+    }, 1*1000)
+}
 
-    triangle.classList = "d-flex justify-content-center text-center"
-    triangle.id = "triangle"
-    triangle.style.padding = "20px"
-    let number = document.querySelector(".number")
+let shakeButton = document.querySelector("#shake")
 
-    // let link = document.createElement("a")
-    // link.href = "url/descarga"
-    // link.target = "_black"
-    // link.onclick()
+shakeButton.onclick = (event) => {
+    console.log(event)
+    shakeBall();
+}
 
-    eightBall.removeChild(number)
-
-    triangle.innerText = new MagicEightBall().generatePrediction()
-    triangleContainer.appendChild(triangle)
-    eightBall.appendChild(triangleContainer)
-}, 3000)
-
-
-
-// eightBall.innerHTML = `<div class="d-flex justify-content-center align-items-center border border-width-1 border-white rounded-circle shadow-sm text-white" style="width: 45%; height: 45%;">
+    
+    // eightBall.innerHTML = `<div class="d-flex justify-content-center align-items-center border border-width-1 border-white rounded-circle shadow-sm text-white" style="width: 45%; height: 45%;">
 //             <div id="triangle" class="d-flex justify-content-center align-items-center">
 //               ${ }
 //             </div>
